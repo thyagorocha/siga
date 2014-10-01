441989dcf8757dbbb7177948423e609ec364d208
Marcos Paulo <marcosfreitascm@gmail.com>
2014-10-01 11:56:53 -0300

    Adicionando verificação do clique do botão para criar novo documento. Caso a página perca o foco ou o clique do botão seja efetuado antes da ação estar habilitada, o clique do botão é tentado novamente.

--- sigaex/src/test/java/br/gov/jfrj/siga/page/objects/PrincipalPage.java
+++ sigaex/src/test/java/br/gov/jfrj/siga/page/objects/PrincipalPage.java
@@ -3,11 +3,14 @@ package br.gov.jfrj.siga.page.objects;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import br.gov.jfrj.siga.integration.test.util.IntegrationTestUtil;
 
 public class PrincipalPage {
 
@@ -16,8 +19,11 @@ public class PrincipalPage {
 	@FindBy(css="a.gt-btn-small.gt-btn-right")
 	private WebElement botaoNovoDocumentoEx;
 	
+	private IntegrationTestUtil util;
+	
 	public PrincipalPage(WebDriver driver) {	
 		this.driver = driver;
+		util = new IntegrationTestUtil();
 		List<WebElement> divs = driver.findElements(By.cssSelector("div.gt-content-box.gt-for-table"));
 		for (WebElement webElement : divs) {
 			new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(webElement));
@@ -27,6 +33,13 @@ public class PrincipalPage {
 	
 	public void clicarBotaoNovoDocumentoEx() {
 		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(botaoNovoDocumentoEx));
+		String URL = driver.getCurrentUrl();
 		botaoNovoDocumentoEx.click();
+		try {
+			new WebDriverWait(driver, 30).until(util.trocaURL(URL));	
+		} catch (TimeoutException e) {
+			System.out.println("TimeOut aguardando a página de novo documento ser exibida. Tentando clicar novamente...");
+			botaoNovoDocumentoEx.click();
+		}
 	}
 }
