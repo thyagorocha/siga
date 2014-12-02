f17cbcf8777fadd4141bb457f197463e9ed52b8b
Markenson <markensonfranca@gmail.com>
2014-12-02 13:41:54 -0200

    Inclui arquivos faltantes no repositorio

--- lib/configure.bat
+++ lib/configure.bat
@@ -7,4 +7,13 @@ call mvn install:install-file -Dfile=WikiParser.jar -DgroupId=ys.wikiparser -Dar
 call mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=hibernate-validator-legacy-3.1.0.GA.jar -DgroupId=org.hibernate -DartifactId=hibernate-validator-legacy -Dversion=3.1.0.GA -Dpackaging=jar
 
+REM echo Disponibilizando as mesmas bibliotecas para upload em servidor remoto
+REM call mvn deploy:deploy-file -Dfile=lib\Qrcode.jar -DgroupId=com.swetake.util -DartifactId=Qrcode -Dversion=1.0.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\ss_css2.jar -DgroupId=com.steadystate.css.parser -DartifactId=ss_css2 -Dversion=1.0.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\jbpm-jpdl.jar -DgroupId=jbpm -DartifactId=jbpm-jpdl -Dversion=1.0.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\jbpm-identity.jar -DgroupId=jbpm -DartifactId=jbpm-identity -Dversion=1.0.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\WikiParser.jar -DgroupId=ys.wikiparser -DartifactId=WikiParser -Dversion=1.0.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+REM call mvn deploy:deploy-file -Dfile=lib\hibernate-validator-legacy-3.1.0.GA.jar -DgroupId=org.hibernate -DartifactId=hibernate-validator-legacy -Dversion=3.1.0.GA -Dpackaging=jar -Durl=file://${project.build.directory}/mvn-repo
+
 echo Libs configuradas com sucesso
